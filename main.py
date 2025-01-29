554e38d8da74b959965866a4705be5c01dad9e20
free <free@example.com>
2025-01-29 15:19:01 -0500

    L11 print a report

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 
+def print_intro(count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document\n")
+
+def print_dico(dico):
+    for char in dico:
+        print(f"'{char}' character was found {dico[char]} times")
+
 def main():
-    #print("hello world")
     path_to_file = "books/frankenstein.txt"
     dico = {}
 
     with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(file_contents)
         word_count = len(file_contents.split())
-        print(word_count)
+        print_intro(word_count)
+
         for char in file_contents:
             key = char.lower()
-            if key in dico:
-                dico[char.lower()]+=1
-            else:
-                dico[char.lower()] = 1
+            if key.isalpha():
+                if key in dico:
+                    dico[char.lower()]+=1
+                else:
+                    dico[char.lower()] = 1
 
-        print(dico)
+
+        print_dico(dico)
 main()
